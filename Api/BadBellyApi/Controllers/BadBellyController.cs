b7f20464e8659a154ced80914baf0d6f65b5a801
TomasCorwell1066 <R.A.Grundy@live.co.uk>
2020-07-10 15:32:37 +0100

    Initial Check in with returning of dummy data on simple get method. Added SwaggerAPI for easy testing.

---
+++ Api/BadBellyApi/Controllers/BadBellyController.cs
@@ -0,0 +1,41 @@
+﻿namespace BadBellyApi.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+
+    using Models;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class BadBellyController : ControllerBase
+    {
+        private readonly ILogger<BadBellyController> _logger;
+
+        public BadBellyController(ILogger<BadBellyController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Ingredient> GetMostIntolerantIngredients()
+        {
+            var rng = new Random();
+            return new List<Ingredient>
+            {
+                new Ingredient
+                {
+                    Name = "Cheese",
+                    IntoleranceOutOfTen = 6
+                },
+                new Ingredient
+                {
+                    Name = "Gluten",
+                    IntoleranceOutOfTen = 3
+                }
+            };
+        }
+    }
+}
---
+++ Api/BadBellyApi/Models/Ingredient.cs
@@ -0,0 +1,9 @@
+namespace BadBellyApi.Models
+{
+    public class Ingredient
+    {
+        public string Name { get; set; }
+
+        public int IntoleranceOutOfTen { get; set; }
+    }
+}
